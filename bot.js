49707b161c5c458c8698622a287df40ae4c7f124
LunarBot <37568058+LunarBot@users.noreply.github.com>
2018-12-27 19:28:18 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,11 @@ client.on('ready', () => {
 client.on('message', message => {
     if (message.content === 'ping') {
     	message.reply('pong');
-  	}
+  	
+    if (message.content === 'hi')
+        message.replay('pong');
+    
+    }
 });
 
 client.login(process.env.BOT_TOKEN);
